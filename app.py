c39b35a63896f372358eea80420087db6eda3f54
DUSHIMIMANA Jean Baptiste <pantherbadou98@gmail.com>
2023-11-09 17:47:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,50 +2,54 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="NISR Dashboard", layout="wide")
-st.title("NISR Hackton Dashboard")
-uploaded_file = st.sidebar.file_uploader("Upload a file")
 
 @st.cache
-def load_data(file, sheet_names=None):
+def load_data(file_url, sheet_names=None):
     if sheet_names is None:
-        data_combine = pd.read_excel(file)
+        data_dict = pd.read_excel(file_url, sheet_name=None)
     else:
-        data_dict = pd.read_excel(file, sheet_name=sheet_names)
-        data_combine = pd.concat(data_dict.values(), ignore_index=True)
+        data_dict = pd.read_excel(file_url, sheet_name=sheet_names)
+    data_combine = pd.concat(data_dict.values(), ignore_index=True)
     return data_combine
 
-if uploaded_file is None:
-    st.info("Upload a file through config")
+
+def create_line_chart(data, y_column, title):
+    fig = px.line(data, x='Year', y=y_column, title=title)
+    return fig
+
+
+uploaded_file = st.sidebar.file_uploader("Upload a file", type=["xlsx"])
+
+
+st.set_page_config(page_title="NISR Dashboard", layout="wide")
+st.title("NISR Hackton Dashboard")
+
+
+if uploaded_file:
+    st.warning("Please ignore the file upload. Data will be loaded from the provided URL.")
     st.stop()
 
-all_sheet_names = pd.read_excel(uploaded_file, sheet_name=None).keys()
+
+all_sheet_names = pd.read_excel("https://github.com/badou98/NISR_Hackton_Dashboard/raw/main/RWLFS2023Q2.xlsx", sheet_name=None).keys()
 selected_sheets = st.sidebar.multiselect("Select sheets", all_sheet_names)
 
-df_combined = load_data(file=uploaded_file, sheet_names=selected_sheets)
+df_combined = load_data("https://github.com/badou98/NISR_Hackton_Dashboard/raw/main/RWLFS2023Q2.xlsx", sheet_names=selected_sheets)
 
+
+st.subheader("Data Table")
 st.dataframe(df_combined)
 
-# Filter data based on user selection
-years = st.sidebar.slider("Select Years", min_value=df_combined['Year'].min(), max_value=df_combined['Year'].max(), value=(df_combined['Year'].min(), df_combined['Year'].max()))
-selected_countries = st.sidebar.multiselect("Select Countries", df_combined['Country'].unique())
+if selected_sheets:
+    st.subheader("Selected Sheets")
+    for sheet_name in selected_sheets:
+        st.write(f"- {sheet_name}")
 
-filtered_data = df_combined[(df_combined['Year'].between(years[0], years[1])) & (df_combined['Country'].isin(selected_countries))]
 
-# Visualizations
-st.header("Visualizations")
+st.subheader("Line Charts")
+columns = st.columns(len(selected_sheets))
 
-# GDP Visualization
-if 'GDP' in filtered_data.columns:
-    fig_gdp = px.line(filtered_data, x='Year', y='GDP', color='Country', title='GDP Over Years')
-    st.plotly_chart(fig_gdp)
-
-# CPI Visualization
-if 'CPI' in filtered_data.columns:
-    fig_cpi = px.bar(filtered_data, x='Year', y='CPI', color='Country', title='Consumer Price Index Over Years')
-    st.plotly_chart(fig_cpi)
-
-# LFS Visualization
-if 'Labour Force' in filtered_data.columns:
-    fig_lfs = px.scatter(filtered_data, x='Year', y='Labour Force', color='Country', title='Labour Force Survey Over Years')
-    st.plotly_chart(fig_lfs)
+for i, sheet_name in enumerate(selected_sheets):
+    y_column = st.selectbox(f"Select Y-axis for {sheet_name}", df_combined.columns, index=i)
+    title = f"{sheet_name} - {y_column}"
+    fig = create_line_chart(df_combined, y_column, title)
+    columns[i].plotly_chart(fig)
