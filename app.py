9a5bcc00cdbbdeb852a2d66e0ec1021d8e3acff2
DUSHIMIMANA Jean Baptiste <pantherbadou98@gmail.com>
2023-11-09 13:58:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,5 @@ import pandas as pd
 st.set_page_config(page_title="NISR Dashboard", layout="wide")
 st.title("NISR Hackton Dashboard")
 
-@st.cache
-def load_data(file):
-    data = pd.read_excel(file)
-    return data
-
-uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is None:
-    st.info("Upload a file through the file uploader.")
-    st.stop()
-
-df = load_data(uploaded_file)
-st.dataframe(df)
+df = pd.read_excel("RWLFS2023Q2.xlsx")
+print(df)
