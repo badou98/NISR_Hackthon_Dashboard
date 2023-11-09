9afa59c1a65ab9fbeed0995f501971075efa47a6
DUSHIMIMANA Jean Baptiste <pantherbadou98@gmail.com>
2023-11-09 18:13:21 +0200

    Revorking

--- app.py
+++ app.py
@@ -1,55 +1,44 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import plotly as pt
 
-
-@st.cache
-def load_data(file_url, sheet_names=None):
+st.set_page_config(page_title="NISR Dashboard",layout="wide")
+st.title("NISR Hackton Dashboard")
+uploaded_file = st.sidebar.file_uploader("Upload a file")
+@st.cache_data
+def load_data(file, sheet_names=None):
+    
+    sheet_name1 = "Table B.1"
+    sheet_name2 = "Table B.2"
+    sheet_name3 = "Table B.3"
+    sheet_name4 = "Table B.4"
+    sheet_name5 = "Table B.5"
+    sheet_name6 = "Table B.6"
+    sheet_name7 = "Table B.7"
+    sheet_name8 = "Table B.8"
+    sheet_name9 = "Table B.9"
+    sheet_name10 = "Table B.10"
+    sheet_name11 = "Table B.11"
+    sheet_name12 = "Table B.12"
     if sheet_names is None:
-        data_dict = pd.read_excel(file_url, sheet_name=None)
+      data_dict = pd.read_excel(file, sheet_names=None)
+      
     else:
-        data_dict = pd.read_excel(file_url, sheet_name=sheet_names)
-    data_combine = pd.concat(data_dict.values(), ignore_index=True)
+        
+
+       data_dict = pd.read_excel(file, sheet_name=[sheet_name1, sheet_name2, sheet_name3, sheet_name4, sheet_name5, sheet_name6, sheet_name7,sheet_name8,sheet_name9,sheet_name10,sheet_name11,sheet_name12,])
+       data_combine = pd.concat(data_dict.values(), ignore_index=True)
     return data_combine
 
 
-def create_line_chart(data, y_column, title):
-    fig = px.line(data, x='Year', y=y_column, title=title)
-    return fig
 
-
-uploaded_file = st.sidebar.file_uploader("Upload a file", type=["xlsx"])
-
-
-st.set_page_config(page_title="NISR Dashboard", layout="wide")
-st.title("NISR Hackton Dashboard")
-
-
-if uploaded_file:
-    st.warning("Please ignore the file upload. Data will be loaded from the provided URL.")
+if uploaded_file is None:   
+    st.info("Upload a file through config")
     st.stop()
+    
+all_sheet_names = pd.read_excel(uploaded_file, sheet_name=None).keys()
+selected_sheets = st.sidebar.multiselect("select sheets", all_sheet_names)
+    
+df_combined = load_data(file=uploaded_file, sheet_names=selected_sheets)
 
-
-all_sheet_names = pd.read_excel("https://github.com/badou98/NISR_Hackton_Dashboard/raw/main/RWLFS2023Q2.xlsx", sheet_name=None).keys()
-selected_sheets = st.sidebar.multiselect("Select sheets", all_sheet_names)
-
-df_combined = load_data("https://github.com/badou98/NISR_Hackton_Dashboard/raw/main/RWLFS2023Q2.xlsx", sheet_names=selected_sheets)
-
-
-st.subheader("Data Table")
 st.dataframe(df_combined)
-
-if selected_sheets:
-    st.subheader("Selected Sheets")
-    for sheet_name in selected_sheets:
-        st.write(f"- {sheet_name}")
-
-
-st.subheader("Line Charts")
-columns = st.columns(len(selected_sheets))
-
-for i, sheet_name in enumerate(selected_sheets):
-    y_column = st.selectbox(f"Select Y-axis for {sheet_name}", df_combined.columns, index=i)
-    title = f"{sheet_name} - {y_column}"
-    fig = create_line_chart(df_combined, y_column, title)
-    columns[i].plotly_chart(fig)
