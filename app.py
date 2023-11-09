a0dbc4c3e1ea16f348a0870ddd2c92518a89cf23
Gasirabo_jacque <gasirabo@localhost.localdomain>
2023-11-09 12:20:03 -0500

    data frame phase

--- app.py
+++ app.py
@@ -3,4 +3,18 @@ import pandas as pd
 import plotly as pt
 
 st.set_page_config(page_title="NISR Dashboard",layout="wide")
-st.title("NISR Hackton Dashboard!")
+st.title("NISR Hackton Dashboard")
+@st.cache_data
+def load_data(file):
+    data = pd.read_excel(file)
+    return data
+
+uploaded_load = st.file_uploader("choose a file")
+
+if uploaded_file is None:
+    st.info(" Upload a file through config", icon="i")
+    st.stop()
+    
+    
+df = load_data(uploaded_file)
+st.dataframe(df)
